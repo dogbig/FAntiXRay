006387ed5679663e9ffa65d2c07562fecbde727d
Luis <lgpserranegra@hotmail.com>
2012-11-29 13:21:13 -0300

    Better block update, v1.3

--- src/me/FurH/FAntiXRay/listener/FBlockListener.java
+++ src/me/FurH/FAntiXRay/listener/FBlockListener.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import me.FurH.FAntiXRay.FAntiXRay;
 import me.FurH.FAntiXRay.configuration.FConfiguration;
+import net.minecraft.server.WorldServer;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.CraftWorld;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -26,44 +28,30 @@ public class FBlockListener implements Listener {
         Player player = e.getPlayer();
         Location location = e.getBlock().getLocation();
         FConfiguration config = FAntiXRay.getConfiguration();
-        
+
         if (config.disabled_worlds.contains(player.getWorld().getName())) {
             return;
-        }
-
-        List<Player> entities = new ArrayList<>();
-
-        entities.add(player);
-
-        for (Entity ent : player.getNearbyEntities(10, 5, 10)) {
-            if (ent instanceof Player) {
-                Player player2 = (Player)ent;
-                if (player2.hasLineOfSight(player)) {
-                    entities.add(player2);
-                }
-            }
         }
 
         int x = location.getBlockX();
         int y = location.getBlockY();
         int z = location.getBlockZ();
         int radius = config.update_radius;
-        
+
         int engine_mode = config.engine_mode;
 
         for (int a = x-radius; a <= x + radius; a++) {
             for (int b = y-radius; b <= y + radius; b++) {
                 for (int c = z-radius; c <= z + radius; c++) {
                     if (a == x && b == y && c == z) { continue; }
-                    for (Player player2 : entities) {
-                        Block block = location.getWorld().getBlockAt(a, b, c);
-                        if (engine_mode == 0 || engine_mode == 1) {
-                            if (config.hidden_blocks.contains(block.getTypeId())) {
-                                player2.sendBlockChange(block.getLocation(), block.getType(), block.getData());
-                            }
-                        } else {
-                            player2.sendBlockChange(block.getLocation(), block.getType(), block.getData());
+                    Block block = location.getWorld().getBlockAt(a, b, c);
+                    WorldServer worldServer = ((CraftWorld) block.getWorld()).getHandle();
+                    if (engine_mode == 0 || engine_mode == 1) {
+                        if (config.hidden_blocks.contains(block.getTypeId())) {
+                            worldServer.notify(a, b, c);
                         }
+                    } else {
+                        worldServer.notify(a, b, c);
                     }
                 }
             }
