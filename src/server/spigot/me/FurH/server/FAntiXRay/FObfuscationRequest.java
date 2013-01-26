e05ddd814f3a2d07ca1f353d9fb84a6b90ab2478
Luis <lgpserranegra@hotmail.com>
2013-01-26 10:13:42 -0300

    Added server files

---
+++ src/server/spigot/me/FurH/server/FAntiXRay/FObfuscationRequest.java
@@ -0,0 +1,14 @@
+package me.FurH.server.FAntiXRay;
+
+import net.minecraft.server.Packet;
+
+/**
+ *
+ * @author FurmigaHumana
+ */
+public class FObfuscationRequest {
+    
+    public Packet getObfuscated(Packet packet) {
+        return packet;
+    }
+}
